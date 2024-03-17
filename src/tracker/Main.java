9be5e09097c6a23e554f7d4483329f0198aafc12
james <james1.6@hotmail.com>
2024-03-17 16:07:15 +1030

    :white_check_mark: Passing stage 1

--- src/tracker/Main.java
+++ src/tracker/Main.java
@@ -1,7 +1,44 @@
 package tracker;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Learning Progress Tracker");
+        mainMenu();
+    }
+
+    private static void mainMenu() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {//menu
+            Command command = Command.get(scanner.nextLine());
+
+            switch (command) {
+                case EXIT -> {
+                    System.out.println("Bye!");
+                    System.exit(0);
+                }
+                case UNKNOWN -> System.out.println("Unknown command!");
+                case EMPTY -> System.out.println("No input");
+            }
+        }
+    }
+
+    public enum Command {
+        EXIT,
+        UNKNOWN,
+        EMPTY;
+
+        public static Command get(String input) {
+            if (input.matches("^\\s*$")) {
+                return EMPTY;
+            } else {
+                try {
+                    return Command.valueOf(input.toUpperCase());
+                } catch (IllegalArgumentException e) {
+                    return UNKNOWN;
+                }
+            }
+        }
     }
 }
